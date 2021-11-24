cf021baebdc1de522739dee90ddd746fc2cae9e3
jialzhong1 <jialzhong@gmail.com>
2021-11-24 07:43:13 -0800

    Completed update_eye_data view

--- views.py
+++ views.py
@@ -16,37 +16,27 @@ def update_eye_data(request, payload_site_slug):
     I am assuming that the application calls to this view is scheduled in order to allow time for the The Eye to update.
     """
 
-    # if not request.user.is_authenticated: # Assume there's a way to authenticate the requestor
-    #     return HttpResponse(status=500)
-
-# {
-#   "session_id": "e2085be5-9137-4e4e-80b5-f1ffddc25423",
-#   "category": "form interaction",
-#   "name": "submit",
-#   "data": {
-#     "host": "www.consumeraffairs.com",
-#     "path": "/",
-#     "form": {
-#       "first_name": "John",
-#       "last_name": "Doe"
-#     }
-#   },
-#   "timestamp": "2021-01-01 09:15:27.243860"
-# }
+    if not request.user.is_authenticated: # Assume there's a way to authenticate the requestor
+        return HttpResponse(status=500)
 
     try:
         response = urllib.request.urlopen(VALID_PAYLOAD_URLS[payload_site_slug]) 
         data = json.loads(response.read())  # Assuming the data is a JSON list of Events
+        error_list = [] # Gathers a list of dicts of the errors found
 
         with transaction.atomic():
             events = []
             for event in data:
-                events.append
+                errors = check_event_error(event)
+                if errors:
+                    error_list.append(errors)
+                events.append(Event(session_id=event['session_id'], category=event['category'], name=event['name'], data=event['data'], timestamp=event['timestamp'], has_errors=len(error_list) > 0))
+            Event.objects.bulk_create(events)
+            logger.warning('Event errors found: %s' % error_list)
 
         return HttpResponse("Updated")
     except Exception as ex:
-        pass
-        # logger.error(ex)
+        logger.error(ex)
 
 
 async def get_event_info(request):
@@ -60,13 +50,13 @@ def check_event_error(event):
     errors = {}
     category = event.get('category', None)
     name = event.get('name', None)
-    time = event.get('timestamp', None)
+    timestamp = event.get('timestamp', None)
 
     now = datetime.now()
 
     if not name:
         errors['name'] = 'None'
-    if not time:
+    if not timestamp:
         errors['timestamp'] = 'None'
 
     if not category:
@@ -76,7 +66,7 @@ def check_event_error(event):
     elif name and name not in VALID_CATEGORIES[category]['valid_names']:
         errors['name'] = 'Invalid name in category'
     
-    if time and datetime.strptime(time, '%Y-%m-%d %H:%M:%S.%f') > datetime.now():
+    if timestamp and datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S.%f') > datetime.now():
         errors['timestamp'] = 'Invalid time'
 
     if errors:
